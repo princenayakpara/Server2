851378446707ba0a5a37afc58881b976b5020f59
princenayakpara <prince.nayakpara.cg@gmail.com>
2026-02-04 11:54:34 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("test");
+const express = require("express");
+
+const app = express ( );
+
+app.get("/", (req, res) => {
+res.send("Express server is running");
+});
+
+app.get("/test/:user", (req,res) => {
+    res.send(req.params.user);
+    console.log(req.params.user);
+});
+
+app.get("use/:one", (req,res) => {
+    res.send("first code");
+});
+app.get("use/one", (req,res) => {
+    res.send("second code");
+});
+app.listen(3000, () => {
+console. log("Server started on port 3000");
+});
