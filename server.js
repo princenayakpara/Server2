30db7cb263d4bbe87098000fd95810bc5e33a426
princenayakpara <prince.nayakpara.cg@gmail.com>
2026-02-17 15:30:49 +0530

    complete

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ let users = [
   { att: "90", uid: 108244, total_sub: 13, bonus: "17", name: "Parth" }
 ];
 
+
 app.get("/", (req, res) => {
   res.send("Express server is running");
 });
 
+
 app.get("/users", (req, res) => {
-  res.status(200).json(users);
+  res.json(users);
 });
 
 app.get("/user/:uid", (req, res) => {
@@ -42,11 +44,39 @@ app.put("/user/:uid", (req, res) => {
 
   if (index === -1) return res.status(404).json({ message: "User not found" });
 
-  users[index] = { ...users[index], ...req.body };
+  users[index] = req.body;
 
   res.json({
-    message: "User updated",
+    message: "User replaced",
     user: users[index]
+  });
+});
+
+app.patch("/user/:uid", (req, res) => {
+  const uid = parseInt(req.params.uid);
+  const user = users.find(u => u.uid === uid);
+
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  Object.assign(user, req.body);
+
+  res.json({
+    message: "User partially updated",
+    user
+  });
+});
+
+app.delete("/user/:uid", (req, res) => {
+  const uid = parseInt(req.params.uid);
+  const index = users.findIndex(u => u.uid === uid);
+
+  if (index === -1) return res.status(404).json({ message: "User not found" });
+
+  const deletedUser = users.splice(index, 1);
+
+  res.json({
+    message: "User deleted",
+    user: deletedUser[0]
   });
 });
 
