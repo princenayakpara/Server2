fcc1b3dd5f9516d2389e07ffa4f2c77242ea8b3a
princenayakpara <prince.nayakpara.cg@gmail.com>
2026-02-17 15:20:12 +0530

    updated

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let users = [
+  { att: "80", uid: 108243, total_sub: 14, bonus: "20", name: "Dax" },
+  { att: "85", uid: 108245, total_sub: 12, bonus: "15", name: "Prince" },
+  { att: "90", uid: 108244, total_sub: 13, bonus: "17", name: "Parth" }
+];
+
+app.get("/", (req, res) => {
+  res.send("Express server is running");
+});
+
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.get("/user/:uid", (req, res) => {
+  const uid = parseInt(req.params.uid);
+  const user = users.find(u => u.uid === uid);
+
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  res.json(user);
+});
+
+app.post("/user", (req, res) => {
+  const newUser = req.body;
+
+  users.push(newUser);
+
+  res.status(201).json({
+    message: "User created",
+    user: newUser
+  });
+});
+
+app.put("/user/:uid", (req, res) => {
+  const uid = parseInt(req.params.uid);
+  const index = users.findIndex(u => u.uid === uid);
+
+  if (index === -1) return res.status(404).json({ message: "User not found" });
+
+  users[index] = { ...users[index], ...req.body };
+
+  res.json({
+    message: "User updated",
+    user: users[index]
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
